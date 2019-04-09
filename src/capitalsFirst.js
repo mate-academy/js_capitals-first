7d4e17e8d6b737d6e76e348aa9566272220eb96a
Danylo Mysak <danylomysak@Romans-MBP.mate.academy>
2019-04-09 17:06:48 +0300

    Improved statement

--- src/capitalsFirst.js
+++ src/capitalsFirst.js
@@ -4,9 +4,10 @@
  * Implement capitalsFirst function:
  *
  * Create a function that takes an input String and returns a String, where all
- * the uppercase words of the input String are in front and all the lowercase
- * words at the end. The order of the uppercase and lowercase words should be
- * the order in which they occur.
+ * the words of the input String that start with a capital letter are in front
+ * and all the words that start with a lowercase letter or a non-alphabetic
+ * character are at the end. The words that belong to the same category must be
+ * left in the original order.
  *
  * If a word starts with a number or special character, skip the word and leave
  * it out of the result. Input String will not be empty.
