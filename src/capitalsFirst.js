12cd6e22b8e2b56512d8441106ce6642213c5f21
ghj ghj <StasGostev>
2019-04-10 14:05:21 +0300

    'Sulution_of_Capitals_first_by_Stas_Gostev'

--- src/capitalsFirst.js
+++ src/capitalsFirst.js
@@ -22,6 +22,27 @@
  */
 function capitalsFirst(str) {
   // write code here
+  let strToArray = str.split(' ');
+  let upperArray = [];
+  let lowerArray = [];
+
+  strToArray.forEach(element => {
+    if (isUpper(element[0])) {
+      upperArray.push(element);
+    }
+    if (isLower(element[0])) {
+      lowerArray.push(element);
+    }
+  });
+  return [...upperArray, ...lowerArray].join(' ');
 }
 
+function isUpper(symbol) {
+  return symbol >= 'A' && symbol <= 'Z';
+}
+function isLower(symbol) {
+  return symbol >= 'a' && symbol <= 'z';
+}
+
+capitalsFirst('d2ffa =8du yh Ug7');
 module.exports = capitalsFirst;
