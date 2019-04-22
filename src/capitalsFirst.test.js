a7000f8f2f9fe4532bd0ea6fd42599f2467805ef
gestap <gestap9@gmail.com>
2019-04-22 16:23:36 +0300

    Updated descrition for test. Deleted .DS_Store

--- src/capitalsFirst.test.js
+++ src/capitalsFirst.test.js
@@ -2,42 +2,32 @@
 
 const capitalsFirst = require('./capitalsFirst');
 
-test('should work with common sentence', () => {
+test('Should work when multiple words capitalized', () => {
   expect(capitalsFirst('hey You, Sort me Already!'))
     .toBe('You, Sort Already! hey me');
 });
 
-test('should work with common sentence 2', () => {
-  expect(capitalsFirst('You baby Me and'))
-    .toBe('You Me baby and');
+test('Should work when one word capitalized', () => {
+  expect(capitalsFirst('life gets pretty Sometimes'))
+    .toBe('Sometimes life gets pretty');
 });
 
-test('should work with common sentence 3', () => {
-  expect(capitalsFirst('Life gets pretty Sometimes'))
-    .toBe('Life Sometimes gets pretty');
-});
-
-test('should skip number', () => {
+test('Should skip a word if it starts with a number', () => {
   expect(capitalsFirst('Av3p P=qup fjn act 8+g yjznb eb wy'))
     .toBe('Av3p P=qup fjn act yjznb eb wy');
 });
 
-test('should skip special characters', () => {
-  expect(capitalsFirst('=xupi1 Vmoba Pq &fa asf ?fak(wq .fas'))
-    .toBe('Vmoba Pq asf');
+test('Should skip a word if it starts with a special characters', () => {
+  expect(capitalsFirst('=xupi1 Vmoba Pq &fa asf ?fak(wq .fas abt &uanu'))
+    .toBe('Vmoba Pq asf abt');
 });
 
-test('should skip special characters 2', () => {
-  expect(capitalsFirst('3g0rh Jl 608 $ynp2 lfcxp7 %ah#5k hgknu 6z H2+8k'))
-    .toBe('Jl H2+8k lfcxp7 hgknu');
-});
-
-test('should work when all words lowercase', () => {
+test('Should work when none of the words are capitalized', () => {
   expect(capitalsFirst('cdk z3v 6aw zlh 6gur3'))
     .toBe('cdk z3v zlh');
 });
 
-test('should work when all words uppercase', () => {
+test('Should work when all words are capitalized', () => {
   expect(capitalsFirst('Ahljf W0z9f F@p W3scd 9axp@o $jiad8'))
     .toBe('Ahljf W0z9f F@p W3scd');
 });
