6aebea3a51a59daf431a901bf849b92b295ce408
Volodsher <volodsher@gmail.com>
2019-08-20 23:38:29 +0300

    that's a big one, but it works

--- src/capitalsFirst.js
+++ src/capitalsFirst.js
@@ -1,24 +1,25 @@
 'use strict';
 
-/**
- * Implement capitalsFirst function:
- *
- * Create a function that takes an input String and returns a String, where all
- * the uppercase words of the input String are in front and all the lowercase
- * words at the end. The order of the uppercase and lowercase words should be
- * the order in which they occur.
- *
- * If a word starts with a number or special character, skip the word and leave
- * it out of the result. Input String will not be empty.
- *
- * capitalFirst("hey You, Sort me Already!") === "You, Sort Already! hey me"
- *
- * @param {string} str
- *
- * @returns {string} - string with uppercase words in front
- */
 function capitalsFirst(str) {
-  // write code here
+  const capitalLetters = [];
+  const lowLetters = [];
+  const newArray = [];
+
+  const findWord = function(word) {
+    if (word[0].toLowerCase() === word[0].toUpperCase()) {
+      return false;
+    }
+    if (word[0] === word[0].toUpperCase()) {
+      capitalLetters.push(word);
+    } else if (word[0] === word[0].toLowerCase()) {
+      lowLetters.push(word);
+    };
+  };
+
+  str.split(' ').forEach(findWord);
+  const newString = newArray.concat(capitalLetters, lowLetters).join(' ');
+
+  return newString;
 }
 
 module.exports = capitalsFirst;
