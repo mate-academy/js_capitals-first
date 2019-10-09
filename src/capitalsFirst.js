2fa5c8d24f3083f228e61bf0a040ea194bb2b8d9
ITStan <s.stanislavskaya@gmail.com>
2019-10-09 16:55:09 +0300

    2nd variant with one loop

--- src/capitalsFirst.js
+++ src/capitalsFirst.js
@@ -18,10 +18,24 @@
  * @returns {string} - string with uppercase words in front
  */
 function capitalsFirst(str) {
+  /* Вариант 1.
   const upperCaseArr = str.split(' ').filter(el => el[0].match(/\b[A-Z]/g));
   const lowCaseArr = str.split(' ').filter(el => el[0].match(/\b[a-z]/g));
+  return ([...upperCaseArr, ...lowerCaseArr].join(' ')); */
 
-  return ([...upperCaseArr, ...lowCaseArr].join(' '));
+  // Вариант 2. Свести все в один цикл.
+  const upperCaseArr = [];
+  const lowerCaseArr = [];
+  const arr = str.split(' ');
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].match(/\b^[A-Z]/g)) {
+      upperCaseArr.push(arr[i]);
+    } else if (arr[i].match(/\b^[a-z]/g)) {
+      lowerCaseArr.push(arr[i]);
+    }
+  }
+  return ([...upperCaseArr, ...lowerCaseArr].join(' '));
 }
 
 module.exports = capitalsFirst;
